b496911de8daae37dcb8e6b8336141b678068b05
Magarajothi0910 <amjothi@ngxcorp.com>
2025-10-18 10:58:16 +0530

    udpates on shit

--- shift/utils.py
+++ shift/utils.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 # MongoDB connection and collection getter for item groups
 def get_shift_collection():
     # client = MongoClient("mongodb://admin:YenE580nOOUE6cDhQERP@194.233.78.90:27017/admin?appName=mongosh+2.1.1&authSource=admin&authMechanism=SCRAM-SHA-256&replicaSet=yenerp-cluster")
-    client = AsyncIOMotorClient("mongodb://localhost:27017/")
+    client = AsyncIOMotorClient("mongodb://admin:YenE580nOOUE6cDhQERP@194.233.78.90:27017/admin?appName=mongosh+2.1.1&authSource=admin&authMechanism=SCRAM-SHA-256&replicaSet=yenerp-cluster")
     # client = AsyncIOMotorClient("mongodb://admin:YenE580nOOUE6cDhQERP@194.233.78.90:27017/admin?appName=mongosh+2.1.1&authSource=admin&authMechanism=SCRAM-SHA-256&replicaSet=yenerp-cluster")
     db = client["reactfluttertest"]  # Adjust database name as per your MongoDB setup
     return db['shifts']  
